2e3366ddac03fbef7e666e87c51bae82428246e3
Luca <lkplmz@gmail.com>
2026-02-16 20:28:24 -0500

    Commit

--- server.js
+++ server.js
@@ -4,3 +4,7 @@ const server = http.createServer((req, res) => {
   console.log("request recived");
   res.end("hola mundo");
 });
+
+server.listen(0, () => {
+  console.log(`Server abierto en http://localhost:${server.address().port}`);
+});
